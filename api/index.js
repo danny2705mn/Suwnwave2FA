b48ec9490c445713d192e0323d6617014895b767
danny2705mn <danny2705mn@gmail.com>
2025-07-17 14:40:21 -0400

    Create index.js
    Update to use otpauth URL secret.

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const url = require('url');
+const querystring = require('querystring');
+const speakeasy = require('speakeasy');
+
+export default function handler(req, res) {
+  const otpauthUrl = 'otpauth://totp/sunwave:aaron@therosemountgroup.com?secret=VU52G3USPHZQODCL&issuer=sunwave';
+  const parsedUrl = url.parse(otpauthUrl);
+  const queryParams = querystring.parse(parsedUrl.query);
+
+  const secret = 'VU52G3USPHZQODCL';
+
+  const token = speakeasy.totp({
+    secret: secret,
+    encoding: 'base32'
+  });
+
+  res.status(200).json({ code: token });
+}
